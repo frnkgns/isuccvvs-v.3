bc3614a94aa15f104f13746e6f390418852a9443
Reina Isabelle Abejuro <166028125+reisabelle@users.noreply.github.com>
2024-06-16 12:46:26 +0800

    Update FormAdd.js

--- FormAdd.js
+++ FormAdd.js
@@ -155,7 +155,7 @@ collegeSelect.addEventListener("change", function() {
 
     // Create default option for positionSelect
     var defaultOption = document.createElement("option");
-    defaultOption.text = "Select College";
+    defaultOption.text = "Select Program";
     defaultOption.value = "";
     defaultOption.disabled = true;
     defaultOption.selected = true;
@@ -167,205 +167,60 @@ collegeSelect.addEventListener("change", function() {
     if (selectedOrganization === "CBM") {
         // Add options for organization A
         var option1 = document.createElement("option");
-        option1.text = "SSC PRESIDENT";
-        option1.value = "A";
+        option1.text = "BSBA";
+        option1.value = option1.text;
 
         var option2 = document.createElement("option");
-        option2.text = "SSC VICE-PRESIDENT";
-        option2.value = "B";
+        option2.text = "BSHM";
+        option2.value = option2.text;
 
         var option3 = document.createElement("option");
-        option3.text = "SSC SECRETARY";
-        option3.value = "C";
+        option3.text = "BSTM";
+        option3.value = option3.text;
 
         var option4 = document.createElement("option");
-        option4.text = "SSC TREASURER";
-        option4.value = "D";
+        option4.text = "BSAIS";
+        option4.value = option4.text;
 
         var option5 = document.createElement("option");
-        option5.text = "SSC AUDITOR";
-        option5.value = "E";
+        option5.text = "BSMA";
+        option5.value = option5.text;
 
         var option6 = document.createElement("option");
-        option6.text = "SSC ACCOUNTANT";
-        option6.value = "F";
+        option6.text = "BSE";
+        option6.value = option6.text;
 
         // Append options to positionSelect
-        positionSelect.appendChild(option1);
-        positionSelect.appendChild(option2);
-        positionSelect.appendChild(option3);
-        positionSelect.appendChild(option4);
-        positionSelect.appendChild(option5);
-        positionSelect.appendChild(option6);
+        courseSelect.appendChild(option1);
+        courseSelect.appendChild(option2);
+        courseSelect.appendChild(option3);
+        courseSelect.appendChild(option4);
+        courseSelect.appendChild(option5);
+        courseSelect.appendChild(option6);
 
     } else if (selectedOrganization === "CED") {
         // Add options for organization B
         var option1 = document.createElement("option");
-        option1.text = "SBO COUNCILOR";
-        option1.value = "A";
+        option1.text = "BEED";
+        option1.value = option1.text;
 
         var option2 = document.createElement("option");
-        option2.text = "SBO PRESIDENT";
-        option2.value = "B";
-
-        var option3 = document.createElement("option");
-        option3.text = "SBO VICE-PRESIDENT";
-        option3.value = "C";
-
-        var option4 = document.createElement("option");
-        option4.text = "SBO SECRETARY";
-        option4.value = "D";
-
-        var option5 = document.createElement("option");
-        option5.text = "SBO TREASURER";
-        option5.value = "E";
-
-        var option6 = document.createElement("option");
-        option6.text = "SBO AUDITOR";
-        option6.value = "F";
-
-        var option7 = document.createElement("option");
-        option7.text = "SBO ACCOUNTANT";
-        option7.value = "G";
-
-        var option8 = document.createElement("option");
-        option8.text = "SBO TREASURER";
-        option8.value = "H";
-
-        var option9 = document.createElement("option");
-        option9.text = "SBO AUDITOR";
-        option9.value = "I";
-
-        var option10 = document.createElement("option");
-        option10.text = "SBO PIO";
-        option10.value = "J";
-
-        var option11 = document.createElement("option");
-        option11.text = "SBO BUSINESS MANAGER I";
-        option11.value = "K";
-
-        var option12 = document.createElement("option");
-        option12.text = "SBO BUSINESS MANAGER II";
-        option12.value = "L";
-
-        var option13 = document.createElement("option");
-        option13.text = "SBO 1ST YEAR REPRESENTATIVE";
-        option13.value = "M";
-
-        var option14 = document.createElement("option");
-        option14.text = "SBO 2ND YEAR REPRESENTATIVE";
-        option14.value = "N";
-
-        var option15 = document.createElement("option");
-        option15.text = "SBO 3RD YEAR REPRESENTATIVE";
-        option15.value = "O";
-
-        var option16 = document.createElement("option");
-        option16.text = "SBO 4TH YEAR REPRESENTATIVE";
-        option16.value = "P";
+        option2.text = "BSE";
+        option2.value = option2.text;
         
-
         // Append options to positionSelect
-        positionSelect.appendChild(option1);
-        positionSelect.appendChild(option2);
-        positionSelect.appendChild(option3);
-        positionSelect.appendChild(option4);
-        positionSelect.appendChild(option5);
-        positionSelect.appendChild(option6);
-        positionSelect.appendChild(option7);
-        positionSelect.appendChild(option8);
-        positionSelect.appendChild(option9);
-        positionSelect.appendChild(option10);
-        positionSelect.appendChild(option11);
-        positionSelect.appendChild(option12);
-        positionSelect.appendChild(option13);
-        positionSelect.appendChild(option14);
-        positionSelect.appendChild(option15);
-        positionSelect.appendChild(option16);
+        courseSelect.appendChild(option1);
+        courseSelect.appendChild(option2);
+        
     } else if (selectedOrganization === "CCJE") {
         // Add options for organization B
         var option1 = document.createElement("option");
-        option1.text = "SBO COUNCILOR";
-        option1.value = "A";
-
-        var option2 = document.createElement("option");
-        option2.text = "SBO PRESIDENT";
-        option2.value = "B";
-
-        var option3 = document.createElement("option");
-        option3.text = "SBO VICE-PRESIDENT";
-        option3.value = "C";
-
-        var option4 = document.createElement("option");
-        option4.text = "SBO SECRETARY";
-        option4.value = "D";
-
-        var option5 = document.createElement("option");
-        option5.text = "SBO TREASURER";
-        option5.value = "E";
-
-        var option6 = document.createElement("option");
-        option6.text = "SBO AUDITOR";
-        option6.value = "F";
-
-        var option7 = document.createElement("option");
-        option7.text = "SBO ACCOUNTANT";
-        option7.value = "G";
-
-        var option8 = document.createElement("option");
-        option8.text = "SBO TREASURER";
-        option8.value = "H";
-
-        var option9 = document.createElement("option");
-        option9.text = "SBO AUDITOR";
-        option9.value = "I";
-
-        var option10 = document.createElement("option");
-        option10.text = "SBO PIO";
-        option10.value = "J";
-
-        var option11 = document.createElement("option");
-        option11.text = "SBO BUSINESS MANAGER I";
-        option11.value = "K";
-
-        var option12 = document.createElement("option");
-        option12.text = "SBO BUSINESS MANAGER II";
-        option12.value = "L";
-
-        var option13 = document.createElement("option");
-        option13.text = "SBO 1ST YEAR REPRESENTATIVE";
-        option13.value = "M";
-
-        var option14 = document.createElement("option");
-        option14.text = "SBO 2ND YEAR REPRESENTATIVE";
-        option14.value = "N";
-
-        var option15 = document.createElement("option");
-        option15.text = "SBO 3RD YEAR REPRESENTATIVE";
-        option15.value = "O";
-
-        var option16 = document.createElement("option");
-        option16.text = "SBO 4TH YEAR REPRESENTATIVE";
-        option16.value = "P";
-        
+        option1.text = "BSCrim";
+        option1.value = option1.text;
 
         // Append options to positionSelect
-        positionSelect.appendChild(option1);
-        positionSelect.appendChild(option2);
-        positionSelect.appendChild(option3);
-        positionSelect.appendChild(option4);
-        positionSelect.appendChild(option5);
-        positionSelect.appendChild(option6);
-        positionSelect.appendChild(option7);
-        positionSelect.appendChild(option8);
-        positionSelect.appendChild(option9);
-        positionSelect.appendChild(option10);
-        positionSelect.appendChild(option11);
-        positionSelect.appendChild(option12);
-        positionSelect.appendChild(option13);
-        positionSelect.appendChild(option14);
-        positionSelect.appendChild(option15);
-        positionSelect.appendChild(option16);
+        courseSelect.appendChild(option1);
+        
     } else if (selectedOrganization === "CCSICT") {
         // Add options for organization B
         var option1 = document.createElement("option");
@@ -384,6 +239,47 @@ collegeSelect.addEventListener("change", function() {
         courseSelect.appendChild(option1);
         courseSelect.appendChild(option2);
         courseSelect.appendChild(option3);
+    } else if (selectedOrganization === "IAT"){
+        // Add options for organization B
+        var option1 = document.createElement("option");
+        option1.text = "DAT";
+        option1.value = option1.text;
+
+        var option2 = document.createElement("option");
+        option2.text = "BAT";
+        option2.value = option2.text;
+
+        // Append options to positionSelect
+        courseSelect.appendChild(option1);
+        courseSelect.appendChild(option2);
+
+    } else if (selectedOrganization ==="SAS"){
+        // Add options for organization B
+        var option1 = document.createElement("option");
+        option1.text = "BAELS";
+        option1.value = option1.text;
+
+        var option2 = document.createElement("option");
+        option2.text = "BAPOS";
+        option2.value = option2.text;
+
+        var option3 = document.createElement("option");
+        option3.text = "BSLM";
+        option3.value = option3.text;
+
+        // Append options to positionSelect
+        courseSelect.appendChild(option1);
+        courseSelect.appendChild(option2);
+        courseSelect.appendChild(option3);
+        
+    } else if (selectedOrganization === "PS"){
+        // Add options for organization B
+        var option1 = document.createElement("option");
+        option1.text = "BSItech";
+        option1.value = option1.text;
+
+        // Append options to positionSelect
+        courseSelect.appendChild(option1);
     }
 });
 
